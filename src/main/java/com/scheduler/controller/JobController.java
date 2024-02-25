51666e4a694e6f5214414307fc307bda1f788f75
Sunny <sunnysinghssvk@gmail.com>
2024-02-25 19:57:24 +0530

    Added welcome rest endpoint

---
+++ src/main/java/com/scheduler/controller/JobController.java
@@ -0,0 +1,21 @@
+package com.scheduler.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcomeMessage() {
+        log.info("Triggered API: /job/welcome");
+        String message = "Welcome to the Scheduler Application";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
